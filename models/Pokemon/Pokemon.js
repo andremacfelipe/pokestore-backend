502e0e05ea9983d8e926cfd4e3c73f948135421d
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-06 22:45:01 -0300

    Pokemon model created

---
+++ models/Pokemon/Pokemon.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+//Model for PokemonSpecies
+
+const PokemonSchema = mongoose.Schema({
+    pokemonName:{type:String,required:true},
+    pokemonHeight:{type:Number, required:true,},
+    pokemonWeight:{type:Number, required:true},
+    pokemonBaseHp:{type:Number,required:true},
+    pokemonBaseAttack:{type:Number,required:true},
+    pokemonBaseDefense:{type:Number,required:true},
+    pokemonTypes:{type:Array,default:[]},
+})
+
+export default mongoose.model("Pokemon",PokemonSchema)

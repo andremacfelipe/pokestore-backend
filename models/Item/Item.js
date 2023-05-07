a0620b191db689ed90ec6f0f0d0bbe1a3d759d0a
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-07 00:11:53 -0300

    Architeture of the Models changed

---
+++ models/Item/Item.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const ItemSchema = mongoose.Schema({
+    ItemType:{type:String,required:true},
+    ItemTypeCode:{type:String,required:true},
+    ItemName:{type:String,required:true},
+    ItemCode:{type:String,required:true},
+    ItemPic:{type:String,required:true}
+})
+
+export default mongoose.model("Item",ItemSchema)

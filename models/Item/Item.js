a3fc6a1a8c1b56a44b4c56b5403e70a6ed4d31ad
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-16 12:11:35 -0300

    ItemCode removed from Item model

--- models/Item/Item.js
+++ models/Item/Item.js
@@ -4,7 +4,6 @@ const ItemSchema = mongoose.Schema({
     ItemType:{type:String,required:true},
     ItemTypeCode:{type:String,required:true},
     ItemName:{type:String,required:true},
-    ItemCode:{type:String,required:true},
     ItemPic:{type:String,required:true}
 })
 

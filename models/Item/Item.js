8bbfa419a82776c96166c4bbbb8392fd170a1fe7
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-16 14:56:01 -0300

    ItemOwner added to ItemSchema

--- models/Item/Item.js
+++ models/Item/Item.js
@@ -4,7 +4,8 @@ const ItemSchema = mongoose.Schema({
     ItemType:{type:String,required:true},
     ItemTypeCode:{type:String,required:true},
     ItemName:{type:String,required:true},
-    ItemPic:{type:String,required:true}
+    ItemPic:{type:String,required:true},
+    ItemOwner:{type:String,required:true}
 })
 
 export default mongoose.model("Item",ItemSchema)

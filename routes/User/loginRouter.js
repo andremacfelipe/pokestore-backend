9c967488e25d45c45f6477a7237a6d8bdfdf33c8
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-18 20:48:30 -0300

    loginRouter.js and registerRouter.js deleted

--- routes/User/loginRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router,json as jsonBodyParser } from "express";
-import { loginController } from "../../controllers/User/User.js";
-
-const loginRouter = Router()
-
-loginRouter.post("/login",jsonBodyParser(),loginController)
-
-export default loginRouter

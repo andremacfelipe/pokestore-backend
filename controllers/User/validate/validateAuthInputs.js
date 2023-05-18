fe1926e0d2dbdebee8091e8ea3ba82d772541a30
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-18 10:46:58 -0300

    register input validate added to validateAuthInputs

---
+++ controllers/User/validate/validateAuthInputs.js
@@ -0,0 +1,19 @@
+import Joi from "joi"
+
+
+
+
+
+
+const validateRegisterInputs = (data) => {
+    const Schema = Joi.object({
+        username: Joi.string().required().min(2).max(500),
+        email: Joi.string().required().min(5).max(200),
+        password: Joi.string().required().min(8)
+    })
+    return Schema.validate(data)
+}
+
+
+
+export { validateRegisterInputs, }

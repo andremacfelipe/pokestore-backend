2deb3bd4b867b7b2d97ef101c70720a5d777229e
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-18 11:02:38 -0300

    loginController added to User controller

--- controllers/User/User.js
+++ controllers/User/User.js
@@ -1,6 +1,7 @@
 import User from "../../models/User/User.js"
 import bcryptjs from "bcryptjs"
-import { validateRegisterInputs } from "./validate/validateAuthInputs.js"
+import jsonwebtoken from "jsonwebtoken"
+import { validateRegisterInputs, validateLoginInputs } from "./validate/validateAuthInputs.js"
 
 
 
@@ -14,9 +15,9 @@ const registerController = async (req, res) => {
     const currentUserName = await User.findOne({ username: req.body.username })
 
     if (currentUserName) {
-        return res.status(400).json({ message: "Username unavailable", path:"username" })
+        return res.status(400).json({ message: "Username unavailable", path: "username" })
     } else if (currentUserEmail) {
-        return res.status(400).json({ message: "Email already in use", path:"email" })
+        return res.status(400).json({ message: "Email already in use", path: "email" })
     }
     else {
         try {
@@ -40,6 +41,37 @@ const registerController = async (req, res) => {
 }
 
 
+const loginController = async (req, res) => {
+
+    const defaultLoginErrorMessage = { message: "Email or password incorrect" }
+    const { error } = validateLoginInputs(req.body)
+
+    if (error) return res.status(400).json(defaultLoginErrorMessage)
+
+    const currentUser = await User.findOne({ email: req.body.email })
+
+    if (!currentUser) return res.status(400).json(defaultLoginErrorMessage)
+
+    if (bcryptjs.compareSync(req.body.password, currentUser.password)) {
+        const token = await jsonwebtoken.sign({
+            userEmail: currentUser.email,
+            username: currentUser.username,
+            userId: currentUser._id
+        },
+            process.env.TOKEN_PRIVATE_KEY,
+
+            {
+                algorithm: "HS512",
+                expiresIn: 60
+            })
+
+        return res.json({ USER_TOKEN: token })
+    } else {
+        return res.status(400).json(defaultLoginErrorMessage)
+    }
+}
 
 
-export {registerController}
+
+
+export { registerController, loginController }

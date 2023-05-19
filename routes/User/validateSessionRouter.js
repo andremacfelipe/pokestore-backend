f9c3e0f7a3cc9c435d8acceedce9e6459805b0a6
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-18 21:10:10 -0300

    controllers/User/validateSessionController.js and routes/User/validateSessionRouter.js deleted

--- routes/User/validateSessionRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router, json as jsonBodyParser } from "express";
-import { validateSessionController } from "../../controllers/User/validateSessionController.js";
-
-const validateSessionRouter = Router()
-
-validateSessionRouter.get("/session/validate",jsonBodyParser(),validateSessionController)
-
-export default validateSessionRouter

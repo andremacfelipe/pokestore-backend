bb7f56d47b13b161af03c793bb9c762e99f5fd39
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-18 21:04:40 -0300

    validateSession added to User controller

---
+++ controllers/User/validate/validateSession.js
@@ -0,0 +1,12 @@
+import jsonwebtoken from "jsonwebtoken"
+
+const validateSession = async (req,res) => {
+    try {
+        const userData = jsonwebtoken.verify(req.headers.user_token,process.env.TOKEN_PRIVATE_KEY)
+        return res.json(userData)
+    } catch (error) {
+        return res.status(401).json({message:"Session invalid"})
+    }
+}
+
+export default validateSession

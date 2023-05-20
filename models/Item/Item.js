4fd8e3684d6b37a820a16e0d20a1fe55a1e96827
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-20 13:16:58 -0300

    camelCase added do ItemSchema

--- models/Item/Item.js
+++ models/Item/Item.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const ItemSchema = mongoose.Schema({
-    ItemType:{type:String,required:true},
-    ItemTypeCode:{type:String,required:true},
-    ItemName:{type:String,required:true},
-    ItemPic:{type:String,required:true},
-    ItemOwner:{type:String,required:true}
+    itemType:{type:String,required:true},
+    itemTypeCode:{type:String,required:true},
+    itemName:{type:String,required:true},
+    itemPic:{type:String,required:true},
+    itemOwner:{type:String,required:true}
 })
 
 export default mongoose.model("Item",ItemSchema)

6c85c5edc63c8ebf1b4b8d96e1e40989c431c9d2
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-20 16:24:13 -0300

    openGenericCase added to Case controller

--- controllers/Case/Case.js
+++ controllers/Case/Case.js
@@ -1,3 +1,39 @@
-import Case from "../../models/Case/Case.js";
-import Pokemon from "../../models/Pokemon/Pokemon.js";
+import User from "../../models/User/User.js";
 
+import Case from "../../models/Case/Case.js";
+
+import { createNewItem } from "../Item/Item.js";
+import { writeCreatedUnits } from "../Pokemon/Pokemon.js";
+
+
+
+const openGenericCase = async (req, res) => {
+
+    const caseDefaultErrorMessage = { message: "Not Found!" }
+
+    const { userId } = req.body.userData
+
+    const currentUser = await User.findOne({ _id: userId })
+    if (!currentUser) return res.status(404).json(caseDefaultErrorMessage) 
+
+    const currentCase = await Case.findOne({name:"Second Generic Case"})
+    if (!currentCase) return res.status(404).json(caseDefaultErrorMessage)
+
+    try {
+
+        const sortedIndex = Math.floor(Math.random() * Number(currentCase.content.length))
+        const sortedPoke = currentCase.content[sortedIndex]
+
+        const newPokemonItem = await createNewItem(currentUser._id,sortedPoke)
+        await writeCreatedUnits(sortedPoke,newPokemonItem._id)
+
+        return res.status(201).json(newPokemonItem)
+
+    } catch (err) {
+        return res.status(400).json(caseDefaultErrorMessage)
+    }
+
+
+}
+
+export { openGenericCase }

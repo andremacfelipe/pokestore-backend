1fc86c4551234a29c2dab10bcfda53b89f2d8bb6
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-20 16:20:00 -0300

    Item model fixed

--- models/Item/Item.js
+++ models/Item/Item.js
@@ -5,7 +5,9 @@ const ItemSchema = mongoose.Schema({
     itemTypeCode:{type:String,required:true},
     itemName:{type:String,required:true},
     itemPic:{type:String,required:true},
-    itemOwner:{type:String,required:true}
+    itemOwner:{type:String,required:true},
+    ownerHistory:[],
+    createdAt:{type:Date, default:Date.now}
 })
 
 export default mongoose.model("Item",ItemSchema)

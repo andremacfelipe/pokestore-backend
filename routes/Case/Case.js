123dd63aa1c2e4ec1ca90918a755968b039a1425
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-20 11:48:41 -0300

    Admin Case routes removed from CaseRouter

--- routes/Case/Case.js
+++ routes/Case/Case.js
@@ -1,18 +1,11 @@
-// caseRouter must be used only on AdminRouter.js
-
 import { Router, json as jsonBodyParser } from "express";
-
-import { getAvailablePokemonSpecies, createNewGenericCase } from "../../controllers/Case/Case.js";
 
 const CaseRouter = Router()
 
 CaseRouter.use(jsonBodyParser())
 
-CaseRouter.get("/cases/pokemon/species",getAvailablePokemonSpecies)
-CaseRouter.post("/cases/new/generic",createNewGenericCase)
 
 
 
 
 export default CaseRouter 
-

f18e577869d8a30910bf2466eaa5852a5a090899
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-20 11:44:05 -0300

    Admin Case Controllers added to Admin controller

--- controllers/Admin/Admin.js
+++ controllers/Admin/Admin.js
@@ -1,7 +1,13 @@
 import User from "../../models/User/User.js";
+import Case from "../../models/Case/Case.js";
+import Pokemon from "../../models/Pokemon/Pokemon.js";
 
 
 
+
+
+
+//Admin User Controllers
 
 const getAllUsers = async (req, res) => {
 
@@ -18,7 +24,6 @@ const getAllUsers = async (req, res) => {
 
 
 }
-
 const handleUsersInfo = (user) => {
     const handledData = 	{
 		id: user._id,
@@ -31,4 +36,49 @@ const handleUsersInfo = (user) => {
     return handledData
 }
 
-export { getAllUsers }
+
+
+
+// Admin Case Controllers
+
+const getAvailablePokemonSpecies = async (req,res) => {
+
+    try {
+        const availablePokes = await Pokemon.find()
+        res.status(200).json(availablePokes)
+    } catch (error) {
+        res.status(404).json({message:"Not found!"})
+    }
+
+
+}
+
+const createNewGenericCase = async (req,res) => {
+
+    const {
+        name,
+        price,
+        content,
+        availableUnits
+    } = req.body
+
+    try {
+        const genericCase = new Case({
+            name,
+            price,
+            content,
+            availableUnits
+        })
+
+        const newGenericCase = await genericCase.save()
+
+        res.status(201).json({message:"Case successfully created!",newGenericCase})
+
+
+    } catch (err) {
+        res.status(400).json({message:"Bad request"})
+    }
+
+}
+
+export { getAllUsers,getAvailablePokemonSpecies, createNewGenericCase }

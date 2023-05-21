50619154ba66d41b3303aa9d929c5161087360ff
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-21 14:23:16 -0300

    purchase route added to user router

--- routes/User/User.js
+++ routes/User/User.js
@@ -2,6 +2,11 @@ import { Router,json as jsonBodyParser } from "express";
 import { loginController ,registerController} from "../../controllers/User/User.js";
 import validateSession from "../../controllers/User/validate/validateSession.js";
 
+import { validateUserAction } from "../../middlewares/validate/User/validateUserAction.js";
+import { validateUserCredits } from "../../middlewares/validate/Case/validateUserCredits.js";
+//
+import { openCase } from "../../controllers/Case/Case.js";
+
 
 const userRouter = Router()
 
@@ -12,6 +17,12 @@ userRouter.post("/register",jsonBodyParser(),registerController)
 //Login
 userRouter.post("/login",jsonBodyParser(),loginController)
 userRouter.get("/session/validate",jsonBodyParser(),validateSession)
+
+
+
+//Purchase
+
+userRouter.post("/purchase/case",jsonBodyParser(),validateUserAction,validateUserCredits,openCase)
 
 
 

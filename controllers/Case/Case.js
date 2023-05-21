fb75710520935d688dfe36e191a94b0842eababa
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-21 14:21:36 -0300

    openCase added to caseControllers

--- controllers/Case/Case.js
+++ controllers/Case/Case.js
@@ -6,24 +6,40 @@ import { createNewItem } from "../Item/Item.js";
 
 
 
-const openGenericCase = async (req, res) => {
+const openCase = async (req, res) => {
 
     const caseDefaultErrorMessage = { message: "Not Found!" }
 
     const { userId } = req.body.userData
+    const caseId = req.query.id
 
     const currentUser = await User.findOne({ _id: userId })
-    if (!currentUser) return res.status(404).json(caseDefaultErrorMessage) 
+    if (!currentUser) return res.status(404).json(caseDefaultErrorMessage)
 
-    const currentCase = await Case.findOne({name:"Second Generic Case"})
-    if (!currentCase) return res.status(404).json(caseDefaultErrorMessage)
+    const currentCase = await Case.findOne({ _id: caseId })
+    if (!currentCase) return res.status(404).json({ message: req.params })
 
     try {
 
         const sortedIndex = Math.floor(Math.random() * Number(currentCase.content.length))
         const sortedPoke = currentCase.content[sortedIndex]
 
-        const newPokemonItem = await createNewItem(currentUser._id,sortedPoke)
+        const newPokemonItem = await createNewItem(currentUser._id, sortedPoke)
+            .then(async (createdItem) => {
+                if (!createdItem) {
+                    throw Error("Item not created")
+                } else {
+                    await User.findOneAndUpdate({ _id: createdItem.itemOwner }, { $inc: { credits: - Number(currentCase.price) } })
+                }
+                currentUser.purchases.push({
+                    itemId: createdItem._id,
+                    date: Date.now()
+                })
+                await currentUser.save()
+                return createdItem
+            }).catch(err => {
+                throw Error("")
+            })
 
         return res.status(201).json(newPokemonItem)
 
@@ -34,4 +50,5 @@ const openGenericCase = async (req, res) => {
 
 }
 
-export { openGenericCase }
+
+export { openCase }

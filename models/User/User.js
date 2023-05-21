bda02f9f441772ccccee029ebd12debf3872415b
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-21 11:42:09 -0300

    min value to user credits added

--- models/User/User.js
+++ models/User/User.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     email:{type:String,required:true,minlength:5,maxlength:200},
     password:{type:String,required:true,minlength:8,},
     purchases:{type:Array,default:[]},
-    credits:{type:Number,default:0},
+    credits:{type:Number,default:0,min:0},
     adress:{type:Object,default:{}},
     roles:{type:Array,default:["user"]},
     

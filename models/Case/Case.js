331fbecee19181b5e5c1342e40d137f2f4f6a192
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-23 02:19:24 -0300

    image src added to Case model

--- models/Case/Case.js
+++ models/Case/Case.js
@@ -4,6 +4,7 @@ const caseSchema = mongoose.Schema({
     name:{type:String,required:true},
     price:{type:Number, required:true},
     content:{type:Array,required:true},
+    image:{type:String,required:true},
     releaseDate:{type:Date, default:Date.now}
 })
 

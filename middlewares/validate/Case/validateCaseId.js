a34b3622c9351c5069a4f22b139eda40f2507cd1
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-25 21:35:33 -0300

    fix:try catch added to validateCaseId middleware

--- middlewares/validate/Case/validateCaseId.js
+++ middlewares/validate/Case/validateCaseId.js
@@ -1,15 +1,20 @@
 import Case from "../../../models/Case/Case.js"
 
-const validateCaseId = async (req,res,next) => {
+const validateCaseId = async (req, res, next) => {
     const caseId = req.query.id
 
-    const currentCase = await Case.findById(caseId)
 
-    if (!currentCase){
-        return res.status(404).json({message:"Not Found!"})
-    }else{
-        next()
+
+    try {
+        const currentCase = await Case.findById(caseId)
+        if (!currentCase) {
+            throw new Error({ message: "Not Found!" })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(404).json({ message: "Case not found!" })
     }
 }
 
-export {validateCaseId}
+export { validateCaseId }

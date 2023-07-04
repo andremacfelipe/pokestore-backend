855700be5b9835bfd88f926727acf8de3d603f0b
andremacfelipe <andremacielfelipe@gmail.com>
2023-07-04 11:27:34 -0300

    added market prop to Item model

--- models/Item/Item.js
+++ models/Item/Item.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
 const ItemSchema = mongoose.Schema({
-    itemType:{type:String,required:true},
-    itemTypeCode:{type:String,required:true},
-    itemName:{type:String,required:true},
-    itemPic:{type:String,required:true},
-    itemOwner:{type:String,required:true},
-    ownerHistory:[],
-    createdAt:{type:Date, default:Date.now}
+    itemType: { type: String, required: true },
+    itemTypeCode: { type: String, required: true },
+    itemName: { type: String, required: true },
+    itemPic: { type: String, required: true },
+    itemOwner: { type: String, required: true },
+    ownerHistory: [],
+    market: {
+        isForSale: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    createdAt: { type: Date, default: Date.now }
 })
 
-export default mongoose.model("Item",ItemSchema)
+export default mongoose.model("Item", ItemSchema)

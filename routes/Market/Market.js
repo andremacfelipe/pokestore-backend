126fbac16f3305a28c54b23705695b325a7ff8c5
andremacfelipe <andremacielfelipe@gmail.com>
2023-07-20 16:54:46 -0300

    changed: getItemsForSale to getMarketListings on /market

--- routes/Market/Market.js
+++ routes/Market/Market.js
@@ -1,10 +1,10 @@
 import { Router, json as jsonBodyParser } from "express";
 import { validateUserAction } from "../../middlewares/validate/User/validateUserAction.js";
-import { getItemsForSale, sellItemInTheMarket } from "../../controllers/Market/Market.js";
+import { getItemsForSale, sellItemInTheMarket,getMarketListings } from "../../controllers/Market/Market.js";
 
 const marketRouter = Router()
 
-marketRouter.get("/market", getItemsForSale)
+marketRouter.get("/market", getMarketListings)
 marketRouter.post("/market/sell/:itemId",jsonBodyParser(),validateUserAction, sellItemInTheMarket)
 
 
